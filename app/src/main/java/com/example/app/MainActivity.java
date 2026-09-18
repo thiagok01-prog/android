83ac9fae525623f78c5d8a5e7a9497421e59848e
thiago <thiago.k01@aluno.ifsc.edu.br>
2026-09-18 12:00:27 -0300

    msg

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int posicao=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Integer[] imagens = new Integer[]{
+                R.drawable.cachorro,
+                R.drawable.gardem,
+                R.drawable.happy,
+                R.drawable.patinho,
+                R.drawable.porquinho
+        };
+        Button botaoVoltar, botaoAvancar;
+        botaoVoltar=findViewById(R.id.btnanterior);
+        botaoAvancar=findViewById(R.id.btnproximo);
+        ImageView imageView=findViewById(R.id.imageView);
+
+        botaoAvancar.setOnClickListener(v -> {
+            posicao++;
+            imageView.setImageResource(imagens[posicao]);
+        });
+
     }
 }
